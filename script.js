81a02938f666b3c56dd5a94d3bd560a1b19606c6
pulsarchief <145131830+pulsarchief@users.noreply.github.com>
2024-07-20 04:49:32 +0530

    with the limited testing I did basic functionality     works ,
    changing a number or operator is massive hassle so both are restricted by code
    
    rest works well , follows lhs to rhs computation , probably done for a while

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const zero = document.querySelector("#zero");
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
+// const zero = document.querySelector("#zero");
+// const one = document.querySelector("#one");
+// const two = document.querySelector("#two");
+// const three = document.querySelector("#three");
+// const four = document.querySelector("#four");
+// const five = document.querySelector("#five");
+// const six = document.querySelector("#six");
+// const seven = document.querySelector("#seven");
+// const eight = document.querySelector("#eight");
+// const nine = document.querySelector("#nine");
 
-const add = document.querySelector("#add");
-const subtract = document.querySelector("#subtract");
-const multiply = document.querySelector("#multiply");
-const divide = document.querySelector("#divide");
+// const add = document.querySelector("#add");
+// const subtract = document.querySelector("#subtract");
+// const multiply = document.querySelector("#multiply");
+// const divide = document.querySelector("#divide");
 
-const display = document.querySelector("#display");
+const main_display = document.querySelector("#main_display");
+const mini_display = document.querySelector("#mini_display");
 
 const clearBtn = document.querySelector("#clear");
 const equalsBtn = document.querySelector("#equals");
@@ -22,28 +23,73 @@ const equalsBtn = document.querySelector("#equals");
 let num1 = "";
 let op = "";
 let num2 = "";
+let display = "";
+
+let num1Input = true;
+let opInput = false;
+let num2Input = false;
+let displayExist = false;
+
+main_display.textContent = "";
+mini_display.textContent = "";
 
 const numList = document.querySelectorAll(".number");
 const operatorList = document.querySelectorAll(".operator");
 
-let turn = 0;
+// execution starts
+
 disableOperatorEqual();
 
-for (const num of numList) {
-    num.addEventListener("click", () => {
-        
+for (const number of numList) {
+    number.addEventListener("click", () => {
+        if (displayExist && opInput) {
+            num1 = display;
+            num2 += number.textContent;
+            num1Input = true;
+            num2Input = true;
+            main_display.textContent += number.textContent;
+        } else if (num1Input && !num2Input && !displayExist && !opInput) {
+            num1 += number.textContent;
+            num1Input = true;
+            main_display.textContent += number.textContent;
+            enableOperatorEqual();
+        } else if (num1Input && opInput) {
+            num2 += number.textContent;
+            main_display.textContent += number.textContent;
+            num2Input = true;
+        }
     });
 }
+
 for (const operator of operatorList) {
     operator.addEventListener("click", () => {
+        if (num1Input && opInput && num2Input) {
+            operation();
+            displayExist = true;
+            mini_display.textContent = display;
+            op = operator.textContent;
+            main_display.textContent += op;
+            num1 = display;
+            num2Input = false;
+            num2 = "";
+        } else if (num1Input && !opInput) {
+            op = operator.textContent;
+            main_display.textContent += op;
+            opInput = true;
+        }
     });
 }
 
 equalsBtn.addEventListener("click", () => {
+    operation();
+    mini_display.textContent = display;
+    main_display.textContent = "";
+    num2 = false;
+    opInput = false;
+    displayExist = true;
 });
 
 clearBtn.addEventListener("click", () => {
-    disableOperatorEqual();
     clear();
 });
 
@@ -61,23 +107,45 @@ function enableOperatorEqual() {
     equals.removeAttribute("disabled");
 }
 
+function disableNumbers() {
+    for (const numbers of numList) {
+        numbers.setAttribute("disabled", "disabled");
+    }
+}
+function enableNumbers() {
+    for (const numbers of numList) {
+        numbers.removeAttribute("disabled");
+    }
+}
+
 function clear() {
-    display.textContent = "";
     num1 = "";
-    op = "";
     num2 = "";
+    display = "";
+    op = "";
+    num1Input = true;
+    opInput = false;
+    num2Input = false;
+    displayExist = false;
+    mini_display.textContent = "";
+    main_display.textContent = "";
+    disableOperatorEqual();
 }
 
 function operation() {
     switch (op) {
         case "+":
-            return addition();
+            display = addition();
+            break;
         case "-":
-            return subtraction();
+            display = subtraction();
+            break;
         case "*":
-            return multiplication();
+            display = multiplication();
+            break;
         case "/":
-            return division();
+            display = division();
+            break;
     }
 }
 
